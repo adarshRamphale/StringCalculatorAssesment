759bd1c071c5974e3b617dca67d73f7c07337e5b
adarshRamphale <adarsh.ramphale@humancloud.co.in>
2025-01-17 17:28:27 +0530

    String Calculator assesment template

---
+++ src/main/java/com/StringCalculatorAssesment/StringCalculatorAssesment.java
@@ -0,0 +1,4 @@
+package com.StringCalculatorAssesment;
+
+public class StringCalculatorAssesment {
+}
---
+++ src/test/java/com/StringCalculatorAssesment/StringCalculatorAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StringCalculatorAssesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
